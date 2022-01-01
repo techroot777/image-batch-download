2563a0cedc6d8160349a2098cbc40fea04e317a5
Antônio Carlos Durães da S ⚡ <37306576+duraes-antonio@users.noreply.github.com>
2021-12-31 23:41:28 -0300

    Limit concurrent requests (#2)
    * UP: Add rxjs, issue downloads progress, limit concurrent requests
    
    * UP: Update package version

--- src/main/adapters/downloader-axios.service.ts
+++ src/main/adapters/downloader-axios.service.ts
@@ -1,14 +1,19 @@
-import { DownloaderService, ImageProcessor, ImageSettings } from '../../core';
+import {
+    DownloaderService,
+    DownloadProgress,
+    ImageProcessor,
+    ImageSettings
+} from '../../core';
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import * as fs from 'fs';
 import * as util from 'util';
 import * as stream from 'stream';
 import { constants } from 'http2';
-import { delayPromise } from '../util';
+import { from, map, mergeMap, Observable, tap } from 'rxjs';
 
 const pipeline = util.promisify(stream.pipeline);
 const DEFAULT_TIMEOUT_MS = 15_000;
-const DEFAULT_DELAY_MS = 25;
+const DEFAULT_PARALLEL_REQUESTS = 8;
 
 export class DownloaderAxiosService implements DownloaderService {
     readonly requestConfig: AxiosRequestConfig = {
@@ -19,16 +24,36 @@ export class DownloaderAxiosService implements DownloaderService {
 
     constructor(private readonly imageProcessor: ImageProcessor) {}
 
-    async batchDownload(
+    batchDownload(
         settings: ImageSettings[],
-        delay: number = DEFAULT_DELAY_MS
-    ): Promise<void[]> {
-        const tasks = settings.map(async (setting, index) => {
-            const delayTime = Math.max(delay ?? DEFAULT_DELAY_MS, 0) * index;
-            await delayPromise(delayTime);
-            return await this.download(setting);
-        });
-        return await Promise.all(tasks);
+        maxParallelRequests: number = DEFAULT_PARALLEL_REQUESTS
+    ): Observable<DownloadProgress> {
+        const total = settings.length;
+        const maxParallelOrDefault =
+            maxParallelRequests ?? DEFAULT_PARALLEL_REQUESTS;
+        let currentIndex = 0;
+
+        const downloadImageAndUpdateCounter = (setting: ImageSettings) =>
+            from(this.download(setting)).pipe(
+                tap(() => ++currentIndex),
+                map(() => setting),
+                map((setting) => ({
+                    lastDownloaded: {
+                        url: setting.imageUrl,
+                        path: setting.outputPath
+                    },
+                    total,
+                    completed: currentIndex,
+                    pending: total - currentIndex
+                }))
+            );
+
+        return from(settings).pipe(
+            mergeMap(
+                (setting) => downloadImageAndUpdateCounter(setting),
+                maxParallelOrDefault
+            )
+        );
     }
 
     async download(settings: ImageSettings): Promise<void> {
